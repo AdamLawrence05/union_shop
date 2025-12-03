f33a20f9e64f81fed17c9d15b2c88a62381104c3
AdamLawrence05 <adam.lawrence59@gmail.com>
2025-12-03 19:34:50 +0000

    Update About Us page content and some styling for improved presentation

--- lib/about_us.dart
+++ lib/about_us.dart
@@ -190,11 +190,33 @@ class AboutUsPage extends StatelessWidget {
             // Body
             Container(
               padding: const EdgeInsets.all(16),
-              child: const Text(
-                'About Us Content Goes Here',
-                style: TextStyle(fontSize: 18),
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                'About Us',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
+              Text( 
+                "Welcome to the Union Shop!" 
+                "We’re dedicated to giving you the very best University branded products, with a range of clothing and merchandise available to shop all year round! We even offer an exclusive personalisation service!"
+                "All online purchases are available for delivery or instore collection!"
+                "We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don’t hesitate to contact us at hello@upsu.net."
+                "Happy shopping! The Union Shop & Reception Team​​​​​​​​​",
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+        ),
+      
+      //footer
       Container(
               width: double.infinity,
               color: Colors.grey[50],
