4a2748ef202c385a7166241e1ca2cac9b6ff11bc
AdamLawrence05 <adam.lawrence59@gmail.com>
2025-12-05 01:01:00 +0000

    add: convert PrintShackPage to StatefulWidget and implement quantity selector

--- lib/pages/print_shack.dart
+++ lib/pages/print_shack.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:union_shop/widgets/navbar.dart';
 import 'package:union_shop/widgets/footer.dart';
 
-class PrintShackPage extends StatelessWidget {
+class PrintShackPage extends StatefulWidget {
   const PrintShackPage({super.key});
+
+  @override
+  State<PrintShackPage> createState() => _PrintShackPageState();
+}
+
+class _PrintShackPageState extends State<PrintShackPage> {
+  int _quantity = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -33,6 +40,57 @@ class PrintShackPage extends StatelessWidget {
                       width: 400,
                       fit: BoxFit.cover,
                     ),
+                  ),
+                  const SizedBox(height: 24),
+                  // Quantity selector
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Quantity:',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black,
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                        child: Row(
+                          children: [
+                            IconButton(
+                              onPressed: _quantity > 1
+                                  ? () => setState(() => _quantity--)
+                                  : null,
+                              icon: const Icon(Icons.remove),
+                              iconSize: 20,
+                              color: const Color(0xFF4d2963),
+                            ),
+                            Container(
+                              width: 50,
+                              alignment: Alignment.center,
+                              child: Text(
+                                '$_quantity',
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                            IconButton(
+                              onPressed: () => setState(() => _quantity++),
+                              icon: const Icon(Icons.add),
+                              iconSize: 20,
+                              color: const Color(0xFF4d2963),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
